a9756bbc959741b16cdba773cbad5a8b5ec59a68
Pranav-Saggam <86381364+Pranav-Saggam@users.noreply.github.com>
2021-09-29 10:23:35 +0530

    Add files via upload

---
+++ JavaScriptAssignment2/data.js
@@ -0,0 +1,77 @@
+let menuList = [{
+        'id': 1,
+        'name': 'french fries',
+        'type': 'snacks',
+        'cost': 150
+    },
+    {
+        'id': 2,
+        'name': 'shawarma',
+        'type': 'snacks',
+        'cost': 220
+    },
+    {
+        'id': 3,
+        'name': 'biryani',
+        'type': 'main',
+        'cost': 300
+    },
+    {
+        'id': 4,
+        'name': 'chicken fry',
+        'type': 'snacks',
+        'cost': 250
+    },
+    {
+        'id': 5,
+        'name': 'paneer curry',
+        'type': 'main',
+        'cost': 320
+    },
+    {
+        'id': 6,
+        'name': 'veg biryani',
+        'type': 'main',
+        'cost': 350
+    },
+    {
+        'id': 7,
+        'name': 'tomato soup',
+        'type': 'starter',
+        'cost': 180
+    },
+    {
+        'id': 8,
+        'name': 'icecream',
+        'type': 'desert',
+        'cost': 150
+    },
+    {
+        'id': 9,
+        'name': 'gulab jamun',
+        'type': 'desert',
+        'cost': 120
+    },
+    {
+        'id': 10,
+        'name': 'mojito',
+        'type': 'beverage',
+        'cost': 150
+    },
+    {
+        'id': 11,
+        'name': 'oreo thickshake',
+        'type': 'beverage',
+        'cost': 170
+    }
+]
+
+/*
+,
+    {
+        'id':,
+        'name':'',
+        'type':'',
+        'cost':
+    }
+*/
